c5f23a549e8458cc3f5a2ae706507e551baef338
ros <ros@roswks03.vertica.dk>
2012-07-27 11:23:20 +0200

    Initial preparations and POC

--- tools/Deploy/Deploy.cmd
+++ tools/Deploy/Deploy.cmd
@@ -23,42 +23,11 @@ IF /I %lastChar%==\  set path2=%path2:~0,-1%
 echo %path1%
 echo %path2%
 
-rem remove umbraco assemblies and its dependencies
-del %1\bin\AjaxControlToolkit.dll
-del %1\bin\businesslogic.dll
-del %1\bin\cms.dll
-del %1\bin\controls.dll
-del %1\bin\CookComputing.XmlRpcV2.dll
-del %1\bin\DotNetOpenMail.dll
-del %1\bin\ICSharpCode.SharpZipLib.dll
-del %1\bin\interfaces.dll
-del %1\bin\Lucene.Net.dll
-del %1\bin\Microsoft.Practices.*.dll
-del %1\bin\Microsoft.ApplicationBlocks.*.dll
-del %1\bin\MySql.Data.dll
-del %1\bin\System.Data.SQLite.dll
-del %1\bin\TidyNet.dll
-del %1\bin\umbraco*.dll
-
-REM Copy in Umbraco 4 specific DLLs
-robocopy "%path1%\..\UCommerce.Umbraco\bin\debug" "%path2%\bin" UCommerce.Umbraco.* PackageActionsContrib.dll
-
-robocopy "%path1%\ucommerce" "%path2%\umbraco\ucommerce" /s /xf *.cs /xf *.??proj /xf *.user /xf *.old /xf *.vspscc /xf xsltExtensions.config /xf web.config /xf uCommerce.key /xf uCommerce.key /xf *.tmp /xd obj /xd _Resharper* /xd .svn /xd _svn	
-
-robocopy "%path1%\bin" "%path2%\bin" /s /xf *.cs /xf *.??proj /xf *.user /xf *.old /xf *.vspscc /xf xsltExtensions.config /xf web.config /xf uCommerce.key /xf uCommerce.key /xf *.tmp /xd obj /xd _Resharper* /xd .svn /xd _svn	
-
-robocopy "%path1%\App_GlobalResources" "%path2%\App_GlobalResources" /s /xf *.cs /xf *.??proj /xf *.user /xf *.old /xf *.vspscc /xf xsltExtensions.config /xf web.config /xf uCommerce.key /xf uCommerce.key /xf *.tmp /xd obj /xd _Resharper* /xd .svn /xd _svn
-
-mkdir "%path2%\umbraco\ucommerce\install\"
-
-REM Copy in install user control
-copy "%path1%\..\UCommerce.Umbraco\Installer\Installer.ascx" "%path2%\umbraco\ucommerce\install\Installer.ascx"
-
-REM Copy in tray icon
-robocopy "%path1%\UCommerce\Images\tray" "%path2%\umbraco\images\tray" UCommerce.gif
-
-REM Pauses the script for a while
-REM PING 1.1.1.1 -n 1 -w 60000 >NUL
+robocopy "%path1%\macroScripts" "%path2%\macroScripts" 
+robocopy "%path1%\css" "%path2%\css"
+robocopy "%path1%\img" "%path2%\img"
+robocopy "%path1%\js" "%path2%\js"
+robocopy "%path1%\masterpages" "%path2%\masterpages"
 
 GOTO :DONE
 
