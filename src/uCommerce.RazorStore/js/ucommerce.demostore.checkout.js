c73d7721af0b8984b145497266544d4d2a88d0d5
Tim Gaunt <tim@thesitedoctor.co.uk>
2012-09-13 16:32:58 +0100

    Changed the layout on the address details page

---
+++ src/uCommerce.RazorStore/js/ucommerce.demostore.checkout.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('form.validate').validate({
+        errorElement: "span",
+        errorClass: "help-inline",
+        highlight: function (label) {
+            $(label).closest('.control-group').addClass('error');
+        },
+        success: function (label) {
+            label.closest('.control-group').addClass('success');
+        }
+    });
+});
