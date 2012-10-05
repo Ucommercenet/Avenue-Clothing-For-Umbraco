619ecc0583a5605c13511442c01b6ef039ed25ad
Tim Gaunt <tim@thesitedoctor.co.uk>
2012-10-05 19:59:39 +0100

    Restructured a number of the folders to better fit the package installer Added installer thank you screen (WIP)

---
+++ src/uCommerce.RazorStore/umbraco/uCommerce/Install/DemoStoreInstaller.ascx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uCommerce.RazorStore.Umbraco.UCommerce.Install
+{
+    public partial class DemoStoreInstaller : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInstall_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- tools/Deploy/Deploy.cmd
+++ tools/Deploy/Deploy.cmd
@@ -31,6 +31,7 @@ robocopy "%path1%\js" "%path2%\js"
 robocopy "%path1%\masterpages" "%path2%\masterpages"
 robocopy "%path1%\bin" "%path2%\bin" uCommerce.RazorStore.dll
 robocopy "%path1%\bin" "%path2%\bin" ServiceStack*.*
+robocopy "%path1%\umbraco\ucommerce\install" "%path2%\umbraco\ucommerce\install" DemoStoreInstaller.ascx
 
 GOTO :DONE
 
