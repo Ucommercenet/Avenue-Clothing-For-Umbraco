19ac41884ec157f9bad5d24e7bd4e108cceb9527
Søren Spelling Lund <devnull@localhost>
2013-02-01 13:26:13 +0100

    Updated build version to 1.0.1.13032.

--- src/uCommerce.RazorStore/Properties/AssemblyInfo.cs
+++ src/uCommerce.RazorStore/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using uCommerce.RazorStore.ServiceStack;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.13032")]
+[assembly: AssemblyFileVersion("1.0.1.13032")]
 [assembly: PreApplicationStartMethod(typeof(ApplicationBase), "Application_Start")]
