634ddb3184615dbee14a24e2a146d3383a24de80
MSK <devnull@localhost>
2013-11-28 12:54:36 +0100

    Added assemblyTag and config for merge.

---
+++ src/uCommerce.RazorStore/Services/AssemblyTag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UCommerce.Web.Api;
+
+namespace UCommerce.RazorStore.Services
+{
+	public class AssemblyTag : IContainsWebservices
+	{
+	}
+}
