9d5467f5e612dcd6fded61f389a0c1d177568817
SÃ¸ren Spelling Lund <ssl@ucommerce.net>
2013-12-20 22:09:57 +0100

    Updated Razor store to v3 to match uCommerce 5 release.

--- src/UCommerce.RazorStore.Installer/Properties/AssemblyInfo.cs
+++ src/UCommerce.RazorStore.Installer/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.13249")]
-[assembly: AssemblyFileVersion("2.0.0.13249")]
+[assembly: AssemblyVersion("3.0.0.13354")]
+[assembly: AssemblyFileVersion("3.0.0.13354")]
