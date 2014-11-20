8c98cf765f2c3036a267fa8fa652725a89c0a9a5
MSK <devnull@localhost>
2014-11-20 10:10:19 +0100

    Updated uCommerce assemblies.'

---
+++ src/uCommerce.RazorStore/umbraco/OrderFlow/InvoicePage.ascx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UCommerce.Presentation.Views.Orders;
+using UCommerce.Presentation.Web.Controls;
+using UCommerce.Presentation.Web.Pages;
+
+namespace UCommerce.RazorStore.umbraco.OrderFlow
+{
+	public partial class InvoicePage : ViewEnabledControl<IEditOrderView>, ISection
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			InvoiceFrame.Src = "/emails/invoice?Orderguid=" + View.Order.OrderGuid;
+		}
+
+		public IList<ICommand> GetCommands()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Show
+		{
+			get { return View.Order.OrderStatus.OrderStatusId == 4; }
+			private set
+			{
+				
+			}
+		}
+	}
+}
