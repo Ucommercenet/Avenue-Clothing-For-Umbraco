d7124780ac7c99b63c628d57492923a7ff236728
SSL <devnull@localhost>
2014-12-05 11:28:42 +0100

    Updated build number to 3.0.6.14339.

--- src/uCommerce.RazorStore/Properties/AssemblyInfo.cs
+++ src/uCommerce.RazorStore/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.0.5.14315")]
-[assembly: AssemblyFileVersion("3.0.5.14315")]
+[assembly: AssemblyVersion("3.0.6.14339")]
+[assembly: AssemblyFileVersion("3.0.6.14339")]
