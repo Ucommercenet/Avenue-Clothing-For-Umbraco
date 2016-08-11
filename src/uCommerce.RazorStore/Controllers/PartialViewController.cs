59368d01cc96eb7ff7f967937f26c21619ec6f78
eaaalpur <devnull@localhost>
2016-08-11 10:45:39 +0200

    Fixed Navigation and dynamic model

--- src/uCommerce.RazorStore/Controllers/PartialViewController.cs
+++ src/uCommerce.RazorStore/Controllers/PartialViewController.cs
@@ -1,51 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using UCommerce.Api;
-//using UCommerce.EntitiesV2;
-//using UCommerce.Extensions;
-//using UCommerce.RazorStore.Models;
-//using Umbraco.Web.Mvc;
-
-//namespace UCommerce.MasterClass.Website.Controllers
-//{
-//	public class PartialViewController : RenderMvcController
-//    {
-//		public ActionResult CategoryNavigation()
-//		{
-//			var categoryNavigation = new CategoryNavigationViewModel();
-
-//			ICollection<Category> rootCategories = UCommerce.Api.CatalogLibrary.GetRootCategories();
-
-//			categoryNavigation.Categories = MapCategories(rootCategories);
-
-//			return View("/views/PartialViews/CategoryNavigation.cshtml", categoryNavigation);
-//		}
-
-//		private IList<CategoryViewModel> MapCategories(ICollection<UCommerce.EntitiesV2.Category> categoriesToMap)
-//		{
-//			var categoriesToReturn = new List<CategoryViewModel>();
-
-//			foreach (var category in categoriesToMap)
-//			{
-//				var categoryViewModel = new CategoryViewModel();
-
-//				categoryViewModel.Name = category.DisplayName();
-//				categoryViewModel.Url = "/store/category?category=" + category.CategoryId;
-////				categoryViewModel.Url = UCommerce.Api.CatalogLibrary.GetNiceUrlForCategory(category);
-//				categoryViewModel.Categories = MapCategories(UCommerce.Api.CatalogLibrary.GetCategories(category));
-
-//				categoriesToReturn.Add(categoryViewModel);
-//			}
-
-//			return categoriesToReturn;
-//		} 
-//	}
-//}
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -80,8 +33,8 @@ namespace UCommerce.MasterClass.Website.Controllers
                 var categoryViewModel = new CategoryViewModel();
 
                 categoryViewModel.Name = category.DisplayName();
-                categoryViewModel.Url = "/store/category?category=" + category.CategoryId;
-                //				categoryViewModel.Url = UCommerce.Api.CatalogLibrary.GetNiceUrlForCategory(category);
+                //categoryViewModel.Url = "/store/category?category=" + category.CategoryId;
+                categoryViewModel.Url = UCommerce.Api.CatalogLibrary.GetNiceUrlForCategory(category);
                 categoryViewModel.Categories = MapCategories(UCommerce.Api.CatalogLibrary.GetCategories(category));
 
                 categoriesToReturn.Add(categoryViewModel);
