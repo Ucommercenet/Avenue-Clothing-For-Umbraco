e1d28e6a99f66bdf631bc1a8212202a4c0c65a64
eaaalpur <devnull@localhost>
2016-08-11 13:33:09 +0200

    Mini Cart component

---
+++ src/uCommerce.RazorStore/Controllers/MiniBasketController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UCommerce.Api;
+using UCommerce.EntitiesV2;
+using Umbraco.Web.Mvc;
+using UCommerce.RazorStore.Models;
+using UCommerce.Runtime;
+
+namespace UCommerce.RazorStore.Controllers
+{
+    public class MiniBasketController : SurfaceController
+    {
+// GET: MiniBasket
+        public ActionResult Index()
+        {
+            var miniBasket = new MiniBasketViewModel();
+            var currency = SiteContext.Current.CatalogContext.CurrentCatalog.PriceGroup.Currency;
+            miniBasket.Total = new Money(0, currency);
+
+            PurchaseOrder basket = null;
+
+            if (TransactionLibrary.HasBasket())
+            {
+                basket = TransactionLibrary.GetBasket(false).PurchaseOrder;
+                if (basket.OrderTotal.HasValue)
+                {
+                    miniBasket.Total = new Money(basket.OrderTotal.Value, currency);
+                    miniBasket.NumberOfItems = basket.OrderLines.Sum(x => x.Quantity);
+                }
+            }
+
+            return View("/views/MiniBasket/MiniBasket.cshtml", miniBasket);
+        }
+    }
+}
+    
---
+++ src/uCommerce.RazorStore/Models/MiniBasketViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UCommerce.RazorStore.Models
+{
+    public class MiniBasketViewModel
+    {      
+        public int NumberOfItems { get; set; }
+        public Money Total { get; set; }
+    }
+}
