64cf75456e720d52246f35c8d7ccfc43f76a1457
eaaalpur <devnull@localhost>
2016-08-12 13:14:45 +0200

    Facets component done.

--- src/uCommerce.RazorStore/Controllers/CategoryController.cs
+++ src/uCommerce.RazorStore/Controllers/CategoryController.cs
@@ -1,62 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using UCommerce.Api;
-//using UCommerce.EntitiesV2;
-//using UCommerce.EntitiesV2.Queries.Marketing;
-//using UCommerce.Extensions;
-//using UCommerce.RazorStore.Models;
-//using UCommerce.Runtime;
-//using UCommerce.Search.Indexers;
-//using Umbraco.Web.Mvc;
-
-//namespace UCommerce.MasterClass.Website.Controllers
-//{
-//	public class CategoryController : RenderMvcController
-//    {
-//		public ActionResult Index()
-//		{
-//			var categoryViewModel = new CategoryViewModel();
-
-//			var currentCategory = UCommerce.Runtime.SiteContext.Current.CatalogContext.CurrentCategory;
-
-//			categoryViewModel.Name = currentCategory.DisplayName();
-//			categoryViewModel.Description = currentCategory.Description();
-//            ////new
-//            //categoryViewModel.Products = MapProducts(UCommerce.Api.CatalogLibrary.GetProducts(category));
-
-//            var productsInCategory = UCommerce.Api.CatalogLibrary.GetProducts(currentCategory);
-
-//			categoryViewModel.Products = MapProducts(productsInCategory);
-
-//			return View("/views/category.cshtml",categoryViewModel);
-//		}
-
-//		private IList<ProductViewModel> MapProducts(ICollection<Product> productsInCategory)
-//		{
-//			IList<ProductViewModel> productViews = new List<ProductViewModel>();
-
-//			foreach (UCommerce.EntitiesV2.Product product in productsInCategory)
-//			{
-//				var productViewModel = new ProductViewModel();
-
-//				productViewModel.Sku = product.Sku;
-//				productViewModel.Name = product.DisplayName();
-//				productViewModel.Url = "/store/product?product=" + product.ProductId;
-
-//				productViewModel.PriceCalculation = UCommerce.Api.CatalogLibrary.CalculatePrice(product);
-
-//				productViews.Add(productViewModel);
-//			}
-
-//			return productViews;
-//		}
-//	}
-//}
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -78,7 +20,7 @@ namespace UCommerce.MasterClass.Website.Controllers
         {
             var categoryViewModel = new CategoryViewModel();
 
-            var currentCategory = UCommerce.Runtime.SiteContext.Current.CatalogContext.CurrentCategory;
+            var currentCategory = SiteContext.Current.CatalogContext.CurrentCategory;
 
             categoryViewModel.Name = currentCategory.DisplayName();
             categoryViewModel.Description = currentCategory.Description();
