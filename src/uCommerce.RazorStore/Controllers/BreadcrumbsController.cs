fd9b7b7677b0cda9024afbd46f217f47184fba95
DavidKelemen <devnull@localhost>
2016-08-15 11:21:17 +0200

    MVC breadcrumbs finished

--- src/uCommerce.RazorStore/Controllers/BreadcrumbsController.cs
+++ src/uCommerce.RazorStore/Controllers/BreadcrumbsController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 using Umbraco.Web.Mvc;
 using UCommerce.Api;
 using UCommerce.EntitiesV2;
 using UCommerce.Extensions;
+using UCommerce.RazorStore.Models;
 using UCommerce.Runtime;
 namespace UCommerce.RazorStore.Controllers
 {
@@ -14,7 +12,32 @@ namespace UCommerce.RazorStore.Controllers
     {
         public ActionResult Index()
         {
-            return View("/Views/PartialView/Breadcrumbs.cshtml");
+           IList<BreadcrumbsViewModel> breadcrumbs = new List<BreadcrumbsViewModel>();
+           Category lastCategory = null;
+           Product product = SiteContext.Current.CatalogContext.CurrentProduct;
+
+            foreach (var category in SiteContext.Current.CatalogContext.CurrentCategories)
+            {
+                var breadcrumb = new BreadcrumbsViewModel()
+                {
+                    BreadcrumbName = category.DisplayName(),
+                    BreadcrumbUrl = CatalogLibrary.GetNiceUrlForCategory(category)
+                };
+                lastCategory = category;
+                breadcrumbs.Add(breadcrumb);
+            }
+
+            if (product != null)
+            {
+                var breadcrumb = new BreadcrumbsViewModel()
+                {
+                    BreadcrumbName = product.DisplayName(),
+                    BreadcrumbUrl = CatalogLibrary.GetNiceUrlForProduct(product, lastCategory)
+                };
+                breadcrumbs.Add(breadcrumb);
+            }
+
+            return View("/Views/PartialView/Breadcrumbs.cshtml", breadcrumbs);
         }
     }
 }
---
+++ src/uCommerce.RazorStore/Models/BreadcrumbsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace UCommerce.RazorStore.Models
+{
+    public class BreadcrumbsViewModel
+    {
+        public BreadcrumbsViewModel()
+        {
+            
+        }
+
+        public string BreadcrumbName { get; set; }
+        public string BreadcrumbUrl { get; set; }
+    }
+}
