79982926d0613b0481b10fa618cd857889756dae
DavidKelemen <devnull@localhost>
2016-08-15 11:58:16 +0200

    Fixed breadcrumbs for paths that are not related to categories and products (ex. Cart)

--- src/uCommerce.RazorStore/Controllers/BreadcrumbsController.cs
+++ src/uCommerce.RazorStore/Controllers/BreadcrumbsController.cs
@@ -6,6 +6,8 @@ using UCommerce.EntitiesV2;
 using UCommerce.Extensions;
 using UCommerce.RazorStore.Models;
 using UCommerce.Runtime;
+using Umbraco.Web;
+
 namespace UCommerce.RazorStore.Controllers
 {
     public class BreadcrumbsController : SurfaceController
@@ -35,6 +37,26 @@ namespace UCommerce.RazorStore.Controllers
                     BreadcrumbUrl = CatalogLibrary.GetNiceUrlForProduct(product, lastCategory)
                 };
                 breadcrumbs.Add(breadcrumb);
+            }
+
+            if (product == null && lastCategory == null)
+            {
+                var currentNode = UmbracoContext.Current.PublishedContentRequest.PublishedContent;
+                foreach (var level in currentNode.Ancestors().Where("Visible"))
+                {
+                    var breadcrumb = new BreadcrumbsViewModel()
+                    {
+                        BreadcrumbName = level.Name,
+                        BreadcrumbUrl = level.Url
+                    };
+                breadcrumbs.Add(breadcrumb);
+                }
+                var currentBreadcrumb = new BreadcrumbsViewModel()
+                {
+                    BreadcrumbName = currentNode.Name,
+                    BreadcrumbUrl = currentNode.Url
+                };
+                breadcrumbs.Add(currentBreadcrumb);
             }
 
             return View("/Views/PartialView/Breadcrumbs.cshtml", breadcrumbs);
