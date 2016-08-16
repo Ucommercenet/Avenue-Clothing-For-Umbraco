9c4ff870c0c3ceba3660b1aec1f7dd544a6e6da8
DavidKelemen <devnull@localhost>
2016-08-16 16:30:07 +0200

    MVC basket done MVC Voucher macro with surface controller done

---
+++ src/uCommerce.RazorStore/Controllers/VoucherController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using umbraco.MacroEngines;
+using UCommerce.Api;
+using UCommerce.RazorStore.Models;
+using Umbraco.Web.Mvc;
+
+namespace UCommerce.RazorStore.Controllers
+{
+    public class VoucherController : SurfaceController
+    {
+        // GET: Voucher
+        public ActionResult Index()
+        {
+            return View("/Views/PartialView/Voucher.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult Index(String voucher)
+        {
+            MarketingLibrary.AddVoucher(voucher);
+            TransactionLibrary.ExecuteBasketPipeline();
+            return Redirect(this.CurrentPage.Url);
+        }
+      
+    }
+}
