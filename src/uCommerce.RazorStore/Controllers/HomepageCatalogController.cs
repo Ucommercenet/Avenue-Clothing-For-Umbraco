a61f20959d430239a1821bf25a0030a56a7fbaac
DavidKelemen <devnull@localhost>
2016-08-23 10:24:36 +0200

    MVC: Homepage Catalog

---
+++ src/uCommerce.RazorStore/Controllers/HomepageCatalogController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UCommerce.Api;
+using UCommerce.Content;
+using UCommerce.Infrastructure;
+using UCommerce.RazorStore.Models;
+using UCommerce.Runtime;
+using Umbraco.Web.Mvc;
+
+namespace UCommerce.RazorStore.Controllers
+{
+    public class HomepageCatalogController : SurfaceController
+    {
+        // GET: HomepageCatalog
+        public ActionResult Index()
+        {
+            var products = SiteContext.Current.CatalogContext.CurrentCatalog.Categories.SelectMany(c => c.Products.Where(p => p.ProductProperties.Any(pp => pp.ProductDefinitionField.Name == "ShowOnHomepage" && Convert.ToBoolean(pp.Value))));
+            ProductsViewModel productsVM = new ProductsViewModel();
+
+            foreach (var p in products)
+            {
+                productsVM.Products.Add(new ProductViewModel()
+                {
+                    Name = p.Name,
+                    PriceCalculation = CatalogLibrary.CalculatePrice(p),
+                    Url = CatalogLibrary.GetNiceUrlForProduct(p),
+                    Sku = p.Sku,
+                    IsVariant = p.IsVariant,
+                    VariantSku = p.VariantSku,
+                    ThumbnailImageUrl = ObjectFactory.Instance.Resolve<IImageService>().GetImage(p.ThumbnailImageMediaId).Url
+                });
+            }
+        
+            return View("/Views/PartialView/HomepageCatalog.cshtml", productsVM);
+        }
+    }
+}
