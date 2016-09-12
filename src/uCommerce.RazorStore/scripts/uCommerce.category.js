9bed4aec7aa6834733cac05de610809f522210f5
DavidKelemen <devnull@localhost>
2016-09-12 13:51:13 +0200

    Async price calculation now includes discounts Price before discount now displayed striked through, with the discounted price underneath.

--- src/uCommerce.RazorStore/scripts/uCommerce.category.js
+++ src/uCommerce.RazorStore/scripts/uCommerce.category.js
@@ -1,28 +1,37 @@
-﻿(function() {
+﻿(function () {
     var categoryId = $('#category-id').val();
     var catalogId = $('#catalog-id').val();
     var i = 0;
     var prices = document.querySelectorAll('[data-productsku]');
+
     for (i ; i < prices.length; i++) {
         var currentPriceElement = prices[i];
         var currentSku = currentPriceElement.dataset.productsku;
         var currentJQelement = $(prices[i]);
 
-        currentJQelement.prepend('<img src="/img/loading-spin.svg" class="product-spinner">');
+        currentJQelement.prepend('<img src="/img/loader.gif" class="product-spinner">');
+
         $.uCommerce.getProductInformation({
-                CatalogId: parseInt(catalogId),
-                CategoryId: parseInt(categoryId),
-                Sku: currentSku
-            },
+            CatalogId: parseInt(catalogId),
+            CategoryId: parseInt(categoryId),
+            Sku: currentSku
+        },
             function (data) {
-                document.querySelector("p[data-productsku='" + data.Sku + "']").innerHTML = data.PriceCalculation.YourPrice.Amount.DisplayValue;
+                if (data.PriceCalculation.IsDiscounted) {
+                    document.querySelector("p[data-productsku='" + data.Sku + "']").innerHTML = data.PriceCalculation.ListPrice.Amount.DisplayValue;
+                    $('#' + data.Sku).find('.item-price').css('text-decoration', 'line-through').css('margin-bottom', '5px');
+                    $('<p class="item-discounted-price">' + data.PriceCalculation.YourPrice.Amount.DisplayValue + '</p>').insertAfter($('#' + data.Sku).find('.item-price'));
+                } else {
+                    document.querySelector("p[data-productsku='" + data.Sku + "']").innerHTML = data.PriceCalculation.YourPrice.Amount.DisplayValue;
+                }
+
                 var product = $('#' + data.Sku);
 
                 var productimage = product.find('.product-image');
                 var productName = product.find('.item-name').find('a');
                 var productButton = product.find('.view-details').find('a');
 
-                productimage.click(function() {
+                productimage.click(function () {
                     window.location = data.NiceUrl;
                 });
 
