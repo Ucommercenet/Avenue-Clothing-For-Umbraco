53f1ecb98d6d23d6da7fef9d2634b030cb7ffc72
DavidKelemen <devnull@localhost>
2016-09-12 11:40:25 +0200

    CSS fix for the button text Added navigation links to product image, name and the 'view more' button

--- src/uCommerce.RazorStore/scripts/uCommerce.category.js
+++ src/uCommerce.RazorStore/scripts/uCommerce.category.js
@@ -1,9 +1,9 @@
 ﻿(function() {
     var categoryId = $('#category-id').val();
     var catalogId = $('#catalog-id').val();
-
+    var i = 0;
     var prices = document.querySelectorAll('[data-productsku]');
-    for (var i = 0; i < prices.length; i++) {
+    for (i ; i < prices.length; i++) {
         var currentPriceElement = prices[i];
         var currentSku = currentPriceElement.dataset.productsku;
         var currentJQelement = $(prices[i]);
@@ -15,8 +15,20 @@
                 Sku: currentSku
             },
             function (data) {
-             
                 document.querySelector("p[data-productsku='" + data.Sku + "']").innerHTML = data.PriceCalculation.YourPrice.Amount.DisplayValue;
+                var product = $('#' + data.Sku);
+
+                var productimage = product.find('.product-image');
+                var productName = product.find('.item-name').find('a');
+                var productButton = product.find('.view-details').find('a');
+
+                productimage.click(function() {
+                    window.location = data.NiceUrl;
+                });
+
+                productimage.css('cursor', 'pointer');
+                productName.attr('href', data.NiceUrl);
+                productButton.attr('href', data.NiceUrl);
             });
     }
 })()
