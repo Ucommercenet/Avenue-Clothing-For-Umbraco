a6305e418fce58485d864b0f17b432fa5210997e
davidKelemen <dak@ucommerce.net>
2018-12-07 14:25:10 +0100

    Shipping and Payment methods now depend on PriceGroup not Currency, so the installer needed to be aligned.

--- src/UCommerce.RazorStore.Installer/umbraco/uCommerce/Install/Helpers/ConfigurationInstaller.cs
+++ src/UCommerce.RazorStore.Installer/umbraco/uCommerce/Install/Helpers/ConfigurationInstaller.cs
@@ -168,10 +168,10 @@ namespace UCommerce.RazorStore.Installer.Helpers
         {
             var shippingMethod = ShippingMethod.SingleOrDefault(x => x.Name == name) ?? new ShippingMethodFactory().NewWithDefaults(name);
 
-            var shippingMethodPrice = shippingMethod.ShippingMethodPrices.FirstOrDefault(p => p.Currency.ISOCode == currency.ISOCode);
+            var shippingMethodPrice = shippingMethod.ShippingMethodPrices.FirstOrDefault(p => p.PriceGroup.Currency.ISOCode == currency.ISOCode);
             if (shippingMethodPrice == null)
             {
-                shippingMethodPrice = new ShippingMethodPrice() { Currency = currency };
+                shippingMethodPrice = new ShippingMethodPrice() { PriceGroup = priceGroup };
                 shippingMethod.AddShippingMethodPrice(shippingMethodPrice);
             }
             shippingMethodPrice.Price = shippingFee;
@@ -203,10 +203,10 @@ namespace UCommerce.RazorStore.Installer.Helpers
             paymentMethod.Deleted = false;
             paymentMethod.FeePercent = feePercentage;
             
-            var method = paymentMethod.PaymentMethodFees.FirstOrDefault(p => p.Currency.ISOCode == currency.ISOCode);
+            var method = paymentMethod.PaymentMethodFees.FirstOrDefault(p => p.PriceGroup.Currency.ISOCode == currency.ISOCode);
             if (method == null)
             {
-                method = new PaymentMethodFee() { Currency = currency };
+                method = new PaymentMethodFee() { PriceGroup = priceGroup };
                 paymentMethod.AddPaymentMethodFee(method);
             }
             method.Fee = fee;
