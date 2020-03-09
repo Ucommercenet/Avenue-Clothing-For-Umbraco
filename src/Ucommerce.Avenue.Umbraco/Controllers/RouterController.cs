e4753091983ed8bb687414a57aeaf43f37dfb424
Petr Ilnytsky <petrilnytsky@gmail.com>
2020-03-09 18:16:50 +0100

    Base for `Category/Product controller`

---
+++ src/Ucommerce.Avenue.Umbraco/Controllers/RouterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Ucommerce.Api;
+using UCommerce.Infrastructure;
+using Umbraco.Web.Mvc;
+
+namespace UCommerce.RazorStore.Controllers
+{
+    public class RouterController : RenderMvcController
+    {
+        public ICatalogContext CatalogContext => ObjectFactory.Instance.Resolve<ICatalogContext>();
+
+        [Route("{*url}")]
+        public ActionResult Index()
+        {
+            var product = CatalogContext.CurrentProduct;
+            
+            if (product != null)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+            return RedirectToAction("Index", "Category");
+        }
+    }
+}
