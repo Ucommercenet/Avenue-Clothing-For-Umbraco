120739400ecfb6bd7a4785538196babbf621ed50
Vitalie Belinschi <vitalie.balinschi@ucommerce.dk>
2020-03-23 20:25:52 +0100

    correct ProductIndexDefinition

--- src/Ucommerce.Avenue.Umbraco/Search/AvenueProductIndexDefinition.cs
+++ src/Ucommerce.Avenue.Umbraco/Search/AvenueProductIndexDefinition.cs
@@ -13,21 +13,10 @@ namespace Ucommerce.Avenue.Umbraco.Search
             this.Field(p => p["CollarSize"], typeof(string));
             this.Field(p => p["ShoeSize"], typeof(string));
             this.Field(p => p["Colour"], typeof(string));
-
+            this.PricesField(p => p.UnitPrices.ToString());
             this.Facet("Colour");
             this.Facet("CollarSize");
             this.Facet("ShoeSize");
-            
-            // TODO: Enable facets on prices once the DrillSidewaysHelpers knows how
-            // this.Facet("UnitPrices", new FacetOptions
-            // {
-            //     Ranges = new List<Range>
-            //     {
-            //         new Range {From = 0.0, To = 100.0},
-            //         new Range {From = 100.0, To = 1000.0},
-            //         new Range {From = 1000.0, To = 10000.0},
-            //     }
-            // });
         }
     }
 }
