e0a0460dc28122530e8fc27a5c67f25daa4afb36
Søren Skovsbøll <soren@skovsboll.com>
2020-05-27 13:27:44 +0200

    fix issue with Color data type not having Editor property

--- src/AvenueClothing.Installer/umbraco/uCommerce/Install/Helpers/ConfigurationInstaller.cs
+++ src/AvenueClothing.Installer/umbraco/uCommerce/Install/Helpers/ConfigurationInstaller.cs
@@ -16,7 +16,7 @@ namespace AvenueClothing.Installer.Helpers
     {
         public IContentTypeService ContentTypeService => Current.Services.ContentTypeService;
         public IContentService ContentService => Current.Services.ContentService;
-        
+
         private Currency _defaultCurrency;
         private PriceGroup _defaultPriceGroup;
         private IList<Country> _countries = new List<Country>();
@@ -81,7 +81,8 @@ namespace AvenueClothing.Installer.Helpers
 
         private void CreateOrderNumberSeries(string name, string prefix)
         {
-            var orderNumberSeries = OrderNumberSerie.SingleOrDefault(o => o.OrderNumberName == name) ?? new OrderNumberSerie();
+            var orderNumberSeries = OrderNumberSerie.SingleOrDefault(o => o.OrderNumberName == name) ??
+                                    new OrderNumberSerie();
             orderNumberSeries.Name = name;
             orderNumberSeries.OrderNumberName = name;
             orderNumberSeries.Deleted = false;
@@ -93,7 +94,8 @@ namespace AvenueClothing.Installer.Helpers
 
         private PriceGroup CreatePriceGroup(string name, Currency currency, decimal vatRate)
         {
-            var priceGroup = PriceGroup.SingleOrDefault(c => c.Name == name) ?? new PriceGroupFactory().NewWithDefaults(name);
+            var priceGroup = PriceGroup.SingleOrDefault(c => c.Name == name) ??
+                             new PriceGroupFactory().NewWithDefaults(name);
             priceGroup.Name = name;
             priceGroup.Currency = currency;
             priceGroup.VATRate = vatRate;
@@ -136,7 +138,6 @@ namespace AvenueClothing.Installer.Helpers
             var emailType = EmailProfileInformation.FirstOrDefault(p => p.EmailType.Name == "OrderConfirmation");
             if (emailType.EmailProfile.EmailContents.All(x => x.CultureCode != "en-US"))
             {
-                
                 EmailContent newEmailContent = new EmailContent();
                 newEmailContent.EmailProfile = emailType.EmailProfile;
                 newEmailContent.CultureCode = "en-US";
@@ -145,9 +146,9 @@ namespace AvenueClothing.Installer.Helpers
 
                 emailType.EmailProfile.EmailContents.Add(newEmailContent);
             }
+
             if (emailType.EmailProfile.EmailContents.All(x => x.CultureCode != "en-GB"))
             {
-
                 EmailContent newEmailContent = new EmailContent();
                 newEmailContent.EmailProfile = emailType.EmailProfile;
                 newEmailContent.CultureCode = "en-GB";
@@ -172,14 +173,18 @@ namespace AvenueClothing.Installer.Helpers
 
         private void CreateShippingMethod(string name, decimal shippingFee, Currency currency, PriceGroup priceGroup)
         {
-            var shippingMethod = ShippingMethod.SingleOrDefault(x => x.Name == name) ?? new ShippingMethodFactory().NewWithDefaults(name);
+            var shippingMethod = ShippingMethod.SingleOrDefault(x => x.Name == name) ??
+                                 new ShippingMethodFactory().NewWithDefaults(name);
 
-            var shippingMethodPrice = shippingMethod.ShippingMethodPrices.FirstOrDefault(p => p.PriceGroup.Currency.ISOCode == currency.ISOCode);
+            var shippingMethodPrice =
+                shippingMethod.ShippingMethodPrices.FirstOrDefault(p =>
+                    p.PriceGroup.Currency.ISOCode == currency.ISOCode);
             if (shippingMethodPrice == null)
             {
                 shippingMethodPrice = new ShippingMethodPrice() { PriceGroup = priceGroup };
                 shippingMethod.AddShippingMethodPrice(shippingMethodPrice);
             }
+
             shippingMethodPrice.Price = shippingFee;
             shippingMethodPrice.PriceGroup = priceGroup;
             shippingMethodPrice.Save();
@@ -189,11 +194,13 @@ namespace AvenueClothing.Installer.Helpers
             {
                 shippingMethod.AddEligibleCountry(country);
             }
+
             shippingMethod.ClearEligibilePaymentMethods();
             foreach (var method in _paymentMethods)
             {
                 shippingMethod.AddEligiblePaymentMethod(method);
             }
+
             shippingMethod.Save();
         }
 
@@ -203,18 +210,22 @@ namespace AvenueClothing.Installer.Helpers
             _paymentMethods.Add(CreatePaymentMethod("Invoice", 0, _defaultCurrency, _defaultPriceGroup, 0));
         }
 
-        private PaymentMethod CreatePaymentMethod(string name, decimal fee, Currency currency, PriceGroup priceGroup, decimal feePercentage)
+        private PaymentMethod CreatePaymentMethod(string name, decimal fee, Currency currency, PriceGroup priceGroup,
+            decimal feePercentage)
         {
-            var paymentMethod = PaymentMethod.SingleOrDefault(x => x.Name == name) ?? new PaymentMethodFactory().NewWithDefaults(name);
+            var paymentMethod = PaymentMethod.SingleOrDefault(x => x.Name == name) ??
+                                new PaymentMethodFactory().NewWithDefaults(name);
             paymentMethod.Deleted = false;
             paymentMethod.FeePercent = feePercentage;
-            
-            var method = paymentMethod.PaymentMethodFees.FirstOrDefault(p => p.PriceGroup.Currency.ISOCode == currency.ISOCode);
+
+            var method =
+                paymentMethod.PaymentMethodFees.FirstOrDefault(p => p.PriceGroup.Currency.ISOCode == currency.ISOCode);
             if (method == null)
             {
                 method = new PaymentMethodFee() { PriceGroup = priceGroup };
                 paymentMethod.AddPaymentMethodFee(method);
             }
+
             method.Fee = fee;
             method.PriceGroup = priceGroup;
             method.Save();
@@ -224,6 +235,7 @@ namespace AvenueClothing.Installer.Helpers
             {
                 paymentMethod.AddEligibleCountry(country);
             }
+
             paymentMethod.Save();
             return paymentMethod;
         }
@@ -256,13 +268,21 @@ namespace AvenueClothing.Installer.Helpers
 
         private DataType CreateDataType(string name, string dataType)
         {
-            var dataTypeEnum = DataType.SingleOrDefault(x => x.TypeName == name) ?? new DataTypeFactory().NewWithDefaults(name);
+            var dataTypeEnum = new DataTypeFactory().NewWithDefaults(name);
 
             dataTypeEnum.TypeName = "Colour";
-            dataTypeEnum.DefinitionName = dataType;
             dataTypeEnum.Nullable = false;
             dataTypeEnum.ValidationExpression = string.Empty;
             dataTypeEnum.BuiltIn = false;
+            dataTypeEnum.Guid = Guid.NewGuid();
+            dataTypeEnum.Properties.Add(new EntityProperty
+            {
+                Guid = Guid.NewGuid(),
+                Version = 1,
+                Value = dataType,
+                EntityId = dataTypeEnum.Guid,
+                DefinitionField = dataTypeEnum.Definition.DefinitionFields.Single(f => f.Name == "Editor"),
+            });
 
             dataTypeEnum.Save();
 
@@ -271,7 +291,10 @@ namespace AvenueClothing.Installer.Helpers
 
         private DataTypeEnum GenerateColourDataTypeEnum(string colour, DataType parentDataType)
         {
-            var dataTypeEnum = DataTypeEnum.SingleOrDefault(x => x.Value == colour && x.DataType.DataTypeId == parentDataType.DataTypeId) ?? new DataTypeEnumFactory().NewWithDefaults(colour);
+            var dataTypeEnum =
+                DataTypeEnum.SingleOrDefault(x =>
+                    x.Value == colour && x.DataType.DataTypeId == parentDataType.DataTypeId) ??
+                new DataTypeEnumFactory().NewWithDefaults(colour);
             dataTypeEnum.Deleted = false;
             dataTypeEnum.DataType = DataType.Get(parentDataType.DataTypeId);
             dataTypeEnum.Save();
@@ -279,7 +302,8 @@ namespace AvenueClothing.Installer.Helpers
             GenericHelpers.DoForEachCulture(language =>
             {
                 if (dataTypeEnum.GetDescription(language) == null)
-                    dataTypeEnum.AddDescription(new DataTypeEnumDescription { CultureCode = language, DisplayName = colour, Description = colour });
+                    dataTypeEnum.AddDescription(new DataTypeEnumDescription
+                        { CultureCode = language, DisplayName = colour, Description = colour });
             });
 
             return dataTypeEnum;
@@ -305,30 +329,39 @@ namespace AvenueClothing.Installer.Helpers
         private void CreateShirtProductDefinition()
         {
             var productDefinition = CreateProductDefinition("Shirt");
-            AddProductDefinitionFieldIfDoesntExist(productDefinition, "CollarSize", "ShortText", true, true, true, "Collar Inches");
+            AddProductDefinitionFieldIfDoesntExist(productDefinition, "CollarSize", "ShortText", true, true, true,
+                "Collar Inches");
             AddProductDefinitionFieldIfDoesntExist(productDefinition, "Colour", "Colour", true, true, true, "Colour");
-	        AddProductDefinitionFieldIfDoesntExist(productDefinition, "ShowOnHomepage", "Boolean", false, false, false, "Show On Homepage");
+            AddProductDefinitionFieldIfDoesntExist(productDefinition, "ShowOnHomepage", "Boolean", false, false, false,
+                "Show On Homepage");
         }
 
         private void CreateShoeProductDefinition()
         {
             var productDefinition = CreateProductDefinition("Shoe");
-            AddProductDefinitionFieldIfDoesntExist(productDefinition, "ShoeSize", "ShortText", true, true, true, "Shoe Size");
-            AddProductDefinitionFieldIfDoesntExist(productDefinition, "ShowOnHomepage", "Boolean", false, false, false, "Show On Homepage");
+            AddProductDefinitionFieldIfDoesntExist(productDefinition, "ShoeSize", "ShortText", true, true, true,
+                "Shoe Size");
+            AddProductDefinitionFieldIfDoesntExist(productDefinition, "ShowOnHomepage", "Boolean", false, false, false,
+                "Show On Homepage");
         }
 
         private void CreateAccessoryProductDefinition()
         {
             var productDefinition = CreateProductDefinition("Accessory");
-            AddProductDefinitionFieldIfDoesntExist(productDefinition, "ShowOnHomepage", "Boolean", false, false, false, "Show On Homepage");
+            AddProductDefinitionFieldIfDoesntExist(productDefinition, "ShowOnHomepage", "Boolean", false, false, false,
+                "Show On Homepage");
         }
 
-        private void AddProductDefinitionFieldIfDoesntExist(ProductDefinition definition, string name, string typeName, bool displayOnWebsite, bool variantProperty, bool promotoToFacet, string displayName)
+        private void AddProductDefinitionFieldIfDoesntExist(ProductDefinition definition, string name, string typeName,
+            bool displayOnWebsite, bool variantProperty, bool promotoToFacet, string displayName)
         {
             if (definition.GetDefinitionFields().Any(f => f.Name == name))
                 return;
 
-            var field = ProductDefinitionField.SingleOrDefault(f => f.Name == name && f.ProductDefinition.ProductDefinitionId == definition.ProductDefinitionId) ?? new ProductDefinitionFieldFactory().NewWithDefaults(name);
+            var field = ProductDefinitionField.SingleOrDefault(f =>
+                            f.Name == name && f.ProductDefinition.ProductDefinitionId ==
+                            definition.ProductDefinitionId) ??
+                        new ProductDefinitionFieldFactory().NewWithDefaults(name);
             field.Name = name;
             field.DataType = DataType.SingleOrDefault(d => d.TypeName == typeName);
             field.Deleted = false;
@@ -336,7 +369,7 @@ namespace AvenueClothing.Installer.Helpers
             field.DisplayOnSite = displayOnWebsite;
             field.IsVariantProperty = variantProperty;
             field.RenderInEditor = true;
-	        field.Facet = promotoToFacet;
+            field.Facet = promotoToFacet;
 
             definition.AddProductDefinitionField(field);
             definition.Save();
