9d4b75ec11ce68c21e6493cb35edabbc6c75294b
Søren Skovsbøll <soren@skovsboll.com>
2020-06-04 10:46:31 +0200

    add membership controller and login form

---
+++ src/AvenueClothing/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AvenueClothing.Models
+{
+    public class LoginViewModel
+    {
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
