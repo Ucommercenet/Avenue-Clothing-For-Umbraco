2e03e3ebc34d8e0b05a9663b23d14a7fad13d578
Søren Skovsbøll <soren@skovsboll.com>
2020-06-08 09:58:23 +0200

    precise selector for log in button

--- src/AvenueClothing/Models/LoginModel.cs
+++ src/AvenueClothing/Models/LoginModel.cs
@@ -5,9 +5,7 @@ namespace AvenueClothing.Models
     public class LoginViewModel
     {
         public string Login { get; set; }
-
         public string Password { get; set; }
-
         public bool RememberMe { get; set; }
     }
 }
