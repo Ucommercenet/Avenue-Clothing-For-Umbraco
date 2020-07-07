d0c676099bd8bbd16db1f0e3388a72d77561008b
Petr Ilnytsky <petrilnytsky@gmail.com>
2020-07-07 13:16:46 +0200

    chore: Update references to Ucommerce 9.2.0

--- src/AvenueClothing/Properties/AssemblyInfo.cs
+++ src/AvenueClothing/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("7.1.0.0")]
-[assembly: AssemblyFileVersion("7.1.0.20148")]
+[assembly: AssemblyVersion("9.2.0.0")]
+[assembly: AssemblyFileVersion("9.2.0.20189")]
