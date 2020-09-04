62f5fa35bdb2e81cd70ebeaeb7516620779c08ba
Søren Skovsbøll <soren@skovsboll.com>
2020-09-04 13:19:01 +0200

    chore: update a few more assembly versions

--- src/AvenueClothing.Installer/Properties/AssemblyInfo.cs
+++ src/AvenueClothing.Installer/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("7.3.0.0")]
-[assembly: AssemblyFileVersion("7.3.0.20248")]
+[assembly: AssemblyFileVersion("7.3.0.20247")]
