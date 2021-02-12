81649a1696f10415183f603eac16be8f5c80b19e
Petr Ilnytsky <petrilnytsky@gmail.com>
2021-02-12 16:11:49 +0100

    feat: make `DisplayName` suggestable
    [ch11186]

--- src/AvenueClothing/Search/AvenueProductIndexDefinition.cs
+++ src/AvenueClothing/Search/AvenueProductIndexDefinition.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using Ucommerce.Search;
-using Ucommerce.Search.Definitions;
+﻿using Ucommerce.Search.Definitions;
 using Ucommerce.Search.Extensions;
-using Ucommerce.Search.Facets;
 
 namespace AvenueClothing.Search
 {
@@ -14,17 +11,26 @@ namespace AvenueClothing.Search
             this.Field(p => p["CollarSize"], typeof(string))
                 .DisplayName("Collar Size")
                 .Facet();
+
             this.Field(p => p["ShoeSize"], typeof(string))
-                .DisplayName("Shoe Size").Facet();
+                .DisplayName("Shoe Size")
+                .Facet();
+
             this.Field(p => p["Colour"], typeof(string))
                 .DisplayName("en-GB", "Colour")
                 .DisplayName("en-US", "Color")
                 .DisplayName("Colour")
                 .Facet();
+
             this.Field(p => p.Taxes);
             this.Field(p => p.PricesInclTax);
             this.Field(p => p.UnitPrices);
-            this.Field(p => p.PricesInclTax["EUR 15 pct"]).Facet().AutoRanges(5, 10);
+            this.Field(p => p.PricesInclTax["EUR 15 pct"])
+                .Facet()
+                .AutoRanges(5, 10);
+
+            this.Field(p => p.DisplayName)
+                .Suggestable();
         }
     }
 }
